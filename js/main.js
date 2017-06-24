66dbb0dfc935a1754738331cb050c13c39fb5dc7
Ryuinferno <ryuinferno.xda@gmail.com>
2017-06-25 01:32:39 +0800

    Highlight sidebar item via JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
 });
+(function(){
+  window.urlParams = {};
+  var e, a = /\+/g,
+      r = /([^&=]+)=?([^&]*)/g,
+      d = function(s) {
+          return decodeURIComponent(s.replace(a, " "));
+      },
+      q = window.location.search.substring(1);
+  while (e = r.exec(q)) {
+    window.urlParams[d(e[1])] = d(e[2]);
+  }
+  if (window.urlParams.device) {
+    $("li#device_" + window.urlParams.device).addClass('active blue');
+    $("li#device_" + window.urlParams.device)
+        .closest('#vendor')
+        .find('.collapsible-header')
+        .addClass('active');
+  } else {
+    $('li#device_all').addClass('active blue');
+  }
+  if (window.urlParams.type) {
+    $("li#type_" + window.urlParams.type).addClass('active blue');
+  } else {
+    $('li#type_all').addClass('active blue');
+  }
+})();
 $('input#search').focus(function(){
   $(this).parent().addClass('focused');
 });
